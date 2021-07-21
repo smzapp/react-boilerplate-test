2d2115fbf0bdf5d5b88b68f7b59657905cee2114
Samuel Amador <samuel.amador@freemight.com>
2021-07-21 13:23:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './slices/store'
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import { transitions, positions, Provider as AlertProvider, types } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
+
+// optional configuration
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 4000,
+  offset: '30px',
+  transition: transitions.FADE,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
